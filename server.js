a3cf32f7221b3cfa4c3e24416ec44471907303e6
Joseph Yoon <jyoonopop@gmail.com>
2022-08-13 09:54:38 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 //middleware
+app.use(express.static("public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
@@ -22,4 +23,9 @@ app.use("/breads", breadsController);
 //listen
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
+});
+
+//404 page
+app.get("*", (req, res) => {
+  res.send("404");
 });
--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -5,14 +5,20 @@ function Default(html) {
     <html>
     <head>
       <title>{html.title || 'Default'}</title>
+      <link rel="stylesheet" href="/main.css" />
     </head>
     <body>
-      <h1>HTML Rendered!</h1>
-      {html.children}
+      <div className="wrapper">
+        <header>
+          <h1><a href="/breads">BreadCRUD</a></h1>
+        </header>
+        <div className="container">
+          {html.children}
+        </div>
+      </div>
     </body>
     </html>
   )
 }
-
 
 module.exports = Default
