db4cbd12aa4121d0ddb4c1deb81577ad6999d250
Joseph Yoon <jyoonopop@gmail.com>
2022-08-15 20:58:43 -0400

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -20,6 +20,7 @@ function Show ({bread, index}) {
                 <li><a href = "/breads">Go home</a></li>
                 <form action={`/breads/${index}?_method=DELETE`} method="POST">
                     <input type='submit' value="DELETE"/>
+                <a href={`/breads/${index}/edit`}><button>Edit</button></a>
                 </form>
 
             </Default>
